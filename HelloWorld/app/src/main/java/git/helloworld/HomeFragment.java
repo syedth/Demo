5d23c53e30908f98bec55fb0681973b64eb8fce5
Syed Thoufiq <syedth.thoufiq@gmail.com>
2017-02-27 22:40:40 +0530

    Navigation Drawer Added

--- HelloWorld/app/src/main/java/git/helloworld/HomeFragment.java
+++ HelloWorld/app/src/main/java/git/helloworld/HomeFragment.java
@@ -2,29 +2,20 @@ package git.helloworld;
 
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
+import git.helloworld.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoggedinFragment extends Fragment {
+public class HomeFragment extends MainActivityFragment {
 
 
-
-    public LoggedinFragment() {
-
-
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -33,7 +24,7 @@ public class LoggedinFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loggedin, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 }
